58bfa904588831f8ca2608438a7a32ed3270adc1
unknown <linrong1121@126.com>
2020-02-09 16:34:55 +0800

    fix

--- account-email/src/main/java/org/weida/toko/email/AccountEmail.java
+++ account-email/src/main/java/org/weida/toko/email/AccountEmail.java
@@ -3,6 +3,6 @@ package org.weida.toko.email;
 import org.weida.toko.exception.AccountEmailException;
 
 public interface AccountEmail {
-
+//stupid
     void sendEmail(String to,String subject,String text) throws AccountEmailException;
 }
